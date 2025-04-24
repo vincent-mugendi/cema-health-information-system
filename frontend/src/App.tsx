3143431fe4d3db1d9fc42c6fbdc11fdccf0b6f15
vincent-mugendi <vincentmwathi5@gmail.com>
2025-04-24 21:08:31 +0300

    Setting up React Router  for app navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "@/components/layout/MainLayout";
 import Dashboard from "@/pages/Dashboard";
+import Clients from "@/pages/Clients";
+import ClientDetail from "@/pages/ClientDetail";
+import Programs from "@/pages/Programs";
+import Enroll from "@/pages/Enroll";
+import NotFound from "@/pages/NotFound";
 
-function App() {
+const queryClient = new QueryClient();
 
-  return (
-    <>
-      <Dashboard />
-      Hello world
-    </>
-  )
-}
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout><Dashboard /></MainLayout>} />
+        <Route path="/clients" element={<MainLayout><Clients /></MainLayout>} />
+        <Route path="/clients/:id" element={<MainLayout><ClientDetail /></MainLayout>} />
+        <Route path="/programs" element={<MainLayout><Programs /></MainLayout>} />
+        <Route path="/enroll" element={<MainLayout><Enroll /></MainLayout>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </HashRouter>
+  </QueryClientProvider>
+);
 
-export default App
+export default App;
